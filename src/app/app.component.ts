21630b67219cebd7fe5578443ea06c41cb138f24
Enes GENÇ <enes.genc@roofstacks.com>
2021-05-23 23:48:35 +0300

    angular ethereum contract example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// @ts-ignore
+import Greeter from '../artifacts/contracts/Greeter.sol/Greeter.json';
+import {ethers} from 'ethers';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ethereum';
+  // Contract Address
+  greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+  greeting: any = '';
+
+  constructor() {
+    console.log('Greeter ABI: ', Greeter.abi);
+  }
+
+  async requestAccount() {
+    // @ts-ignore
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  // call the smart contract, read the current greeting value
+  async fetchGreeting() {
+    // @ts-ignore
+    if (typeof window.ethereum !== 'undefined') {
+      // @ts-ignore
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(this.greeterAddress, Greeter.abi, provider);
+      try {
+        const data = await contract.greet();
+        console.log('data: ', data);
+      } catch (err) {
+        console.log('Error: ', err);
+      }
+    }
+  }
+
+  // call the smart contract, send an update
+  async setGreeting() {
+    if (!this.greeting) { return; }
+    // @ts-ignore
+    if (typeof window.ethereum !== 'undefined') {
+      await this.requestAccount();
+      // @ts-ignore
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(this.greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(this.greeting);
+      await transaction.wait();
+      await this.fetchGreeting();
+    }
+  }
 }
